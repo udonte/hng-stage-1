9b575a282359ac0c32f96a1c4556615a02579bb6
George Udonte <georgeudonte@gmail.com>
2023-09-08 16:31:50 +0100

    added realtime time

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 
 
+
+
+
+// const interval = setInterval(function(){
+//   currentUtcTimePara.textContent = currentUtcTime
+// }, 100)
+
+const interval = setInterval(function() {
 // get the ccurent day
 let currentDayPara = document.getElementById("current_day");
 let allDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 let theDate = new Date();
 let currentDay = allDays[theDate.getDay()];
-currentDayPara.textContent = currentDay;
+currentDayPara.textConten = currentDay;
 
 //get the current utc time
 let currentUtcTimePara = document.getElementById("utc_time");
@@ -19,5 +27,8 @@ let h = addZero(theDate.getUTCHours());
 let m = addZero(theDate.getUTCMinutes());
 let s = addZero(theDate.getUTCSeconds());
 let currentUtcTime = h + ":" + m + ":" + s;
+currentUtcTimePara.innerText = currentUtcTime
+},
+ 1000);
 
-currentUtcTimePara.textContent = currentUtcTime;
+// clearInterval(interval)
